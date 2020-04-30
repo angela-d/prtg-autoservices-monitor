4375e5e81d0df306ea21fa924868812816e96a74
angela <33108490+angela-d@users.noreply.github.com>
2020-04-29 20:20:43 -0500

    Initial commit
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,108 @@
+# PRTG Auto-Start Services Monitor
+An alternative to WMI monitoring method that doesn't require PRTG to have a user account on the target machine or domain admin credentials; with options to auto-start whitelisted services.
+
+This script runs on the *target machine*, rather than the probe device/PRTG and utilizes the PRTG API for sensor alerts.
+
+***
+A service (re)started by this script:
+
+![started service](started-service.png)
+***
+All clear response from the API:
+
+![all services running](all-running.png)
+***
+
+## Features
+- Doesn't require any user account on the target machine
+- Runs via a task on the target machine & communicates only need-to-know info to PRTG via the PRTG API
+- Option to force start monitored services automatically (if they're not running)
+- Debugging option to see exactly what data is seen and posted to PRTG
+
+## Minimum Requirements
+These are not *hard* requirements, necessarily; but haven't been tested on anything but the (minimum) versions below:
+
+Target machine:
+- Windows Server 2016
+- Powershell version: `$PSVersionTable.PSVersion`
+
+```text
+Major  Minor  Build  Revision
+-----  -----  -----  --------
+5      1      14393  3471   
+```
+
+PRTG:
+- Version:  20.1.57.1745+ (check your dashboard footer)
+
+
+## Install / Setup
+- Assess **prtg-autostart-monitor.ps1** code to ensure nothing bad will be done to your system & no data will be stolen upon running it
+- If all checks out, add **prtg-autostart-monitor.ps1** to your target system, somewhere like `C:\Scripts` is usually a good spot
+- On the device (server) you want to monitor these services for, locate it's device (Search > enter name > click to it > + Add Sensor)
+- Search for **HTTP Push Data Advanced**
+ - Sensor Name: Auto-Start Services
+ - SSL Settings: HTTPS
+ - Request Method: POST
+ - No incoming data: Switch to unknown
+ - Create
+
+ Once it's created, you need to go to the device/server's overview and you'll see **Auto-Start** services listed as a new sensor.  
+ - Click to it and go to its **Settings**
+ - You'll see the **Identification Token** field populated, copy it and paste into the `$prtgToken` of the **prtg-autoservices-monitor.ps1** script
+ - Modify the `$prtgUrl` variable and set your PRTG's instance URL
+ - Save
+
+## Setup the Task Schedule
+Using the Windows Task Scheduler GUI
+- On the target machine, open *Task Scheduler*
+- Create a Task (right pane)
+
+
+- **General tab:**
+ - Name: `PRTG Service Monitor`
+ - Description: `Monitor autostart services and report back to PRTG.`
+ - When running the task, use the following user account: click **Change User or Group** > ensure **From this location:** is set to your local machine (not an AD domain) > type `SYSTEM` under the *Enter the object name to select* > click OK
+ - [x] Run with highest privileges
+ - Configure for: `Windows Server 2016`
+
+
+- **Triggers tab:**
+ - Click **New**
+ - [x] Daily
+ - Recur every `1` days
+ - Repeat task every: `1 minute` (or your preferred duration)
+ - for a duration of: `Indefinitely`
+ - [x] Enabled
+
+
+- **Actions tab:**
+ - Click **New**
+ - Program/script: `powershell`
+ - Add arguments: `C:\Scripts\prtg-autoservices-monitor.ps1` (adjust path accordingly, if you put it elsewhere)
+
+
+- **Conditions tab:**
+ - Uncheck all
+
+
+- **Settings tab:**
+ - Tick all & set preferred durations; leave *If the task is not scheduled to run again, delete it after* **blank/unchecked**
+
+## Customizing
+**Services you don't care about**; anything that doesn't have the **Startup Type** of Automatic or Automatic (Delayed) is ignored by default - sometimes Windows likes to add services that aren't integral as automatic startup, so that's mostly what you'd add here.
+- `$ignore` - Separate service names with '', like so: `$ignore = 'Windows Biometric Service','Software Protection'`
+
+**Services that are important and need to be restarted without manual intervention** - note that if this script does its job, you'll never know the service(s) crashed.. if you need a record of what was restarted, don't use **$debug** for that, or you'll rapidly fill your harddisk space - perhaps extend the debug option and only record what's useful to you.
+- `$forceStart` - This variable operates the same way as `$ignore`
+
+**Debugging/testing** or survey what's being sent to PRTG
+- `$debug` - Set to `1` and check the path specified in `$logPath` for your output
+
+## Caveats
+`$debug` is noisy and won't clean up after itself when you switch it off, don't use it unless you're actively trying to test this script!
+
+## Credits
+Original codebase from [Stephan Linke | Paessler AG](https://pastebin.com/SLdtB3gX)
+
+My modifications are licensed under GPL 3
---
+++ prtg-autoservices-monitor.ps1
@@ -0,0 +1,134 @@
+# license GPL3 https://github.com/angela-d/prtg-autoservices-monitor
+# original script base from Stephan Linke | Paessler AG https://pastebin.com/SLdtB3gX
+
+# config
+$prtgUrl    = 'https://prtg.example.com'
+$prtgPort   = '5051'
+$prtgToken  = 'generated-by-prtg-see-install-instructions'
+$debug      = 0 # set to 1 if you want to save a log file to the location referenced in $logPath
+$logPath    = "C:\Scripts\logs\autostart-services-log.txt"
+# if you want to force-start a service, name them here:
+$forceStart = 'syncthing'
+# end config
+
+# services we don't care about (can also disable via services.msc) - separate with '' for services with spaces in the title
+$ignore = 'Windows Biometric Service','Software Protection','Connected Devices Platform Service','Downloaded Maps Manager','Net.Tcp Port Sharing Service','silsvc'
+
+# poll the automatic services
+$services = Get-Service | Where {$_.StartType -eq 'Auto' -and $ignore -notcontains $_.DisplayName -and $_.Status -ne 'Running'}
+
+# debug settings; will write to a log path specified
+function debugger($where, $message) {
+  if ($debug -eq 1 -AND (Test-Path $logPath)) {
+    Write-Output ($where + ": " + $message) | Out-File $logPath -Append
+  } elseif ($debug -eq 1 -AND !(Test-Path $logPath)) {
+    New-Item $logPath -ItemType file
+
+    # redundant, but add-content was goobering up with unnecessary spaces between letters
+    Write-Output ($where + ": " + $message) | Out-File $logPath -Append
+
+    # add a timestamp and newline returns to make the output more readable
+    Write-Output "===^ Debug timestamp $((Get-Date).ToString("MM-dd-yy hh:mm:ss")) ===`r`n" | Out-File $logPath -Append
+  }
+}
+
+# check a service's status
+function Check-ServiceStats($service) {
+	if ((Get-Service $service).Status) {
+		$serviceStatus = (Get-Service -Name $service).Status
+		debugger "$service status check" "$service is $serviceStatus"
+	} else {
+		debugger "$service status check" "$service not found or is not running!"
+	}
+    return $serviceStatus
+}
+
+# restart/start a service
+function StartService($serviceName) {
+  debugger "Restart status check" "Preparing to restart $serviceName"
+  Get-Service -Name $serviceName | Start-Service -ErrorAction SilentlyContinue
+}
+
+# when called, this function compiles data to notify prtg of happenings
+function prtgApi($alertText,$alertValue) {
+  $header = @{
+    "Content-Type"  = "application/json";
+  }
+
+  # channel will be created if it doesn't already exist
+  # converting result to an array in the hashtable syntax will ensure the json data is formatted as prtg expects it
+  $prtgAlertBody = @{
+    prtg = @{
+    error = $alertValue;
+    result = @(
+      @{
+        channel = 'Services Not Running'
+        value = $alertValue
+      })
+    text = $alertText;
+    }
+  }
+
+  # specify a conversion depth cause we have a lot of nested data, without, it'll output
+  # System.Collections.Hashtable for object names and doesn't expand the hashtable values
+  $alert = ConvertTo-Json -Depth 4($prtgAlertBody)
+  debugger "JSON Sent to PRTG" $alert
+
+  $prtgPayloadUrl = ($prtgUrl + ":" + $prtgPort + "/" + $prtgToken)
+  debugger "Preparing API connection" $PRTGpayloadUrl
+
+  # final step.. notify prtg
+  Invoke-RestMethod -Method Post -Uri $prtgPayloadUrl -Headers $header -Body $alert
+}
+
+# check on services with the autostart startup type
+if($services){
+  $serviceList = ($services | Select -expand DisplayName) -join ", "
+  $serviceCount = $services.Count
+
+  # problems detected
+  if ($serviceCount -ne '0') {
+    $alertText = ("Automatic service(s) not running: " + $serviceList)
+    debugger "Error condition met" $alertText
+
+    # notify PRTG some services aren't active; 1 = error trigger
+    prtgApi $alertText 1
+  }
+
+} elseif (!$serviceList) {
+  # all services appear active
+  $alertText = "All auto-start services are running"
+  debugger "Stopped autostart service count = $serviceCount, all are running" $alertText
+
+  # notify PRTG all auto services are active; 0 = OK
+  prtgApi $alertText 0
+
+} else {
+    $alertText = ("Unable to get service list; check Event Viewer")
+    debugger "Empty result" $alertText
+    prtgApi $alertText 1
+  }
+
+# check force start prefs.. if the service is on ignore, it won't be matched!
+if ($services) {
+  foreach ($serviceToStart in $forceStart)  {
+    debugger "Checking force start option for" $serviceToStart
+
+    if ($serviceList.contains($serviceToStart)){
+      debugger "Force Start Service Inactive.. about to start" $serviceToStart
+
+      StartService $serviceToStart
+      $confirmStatus = Check-ServiceStats $serviceToStart
+
+      if ($confirmStatus -eq 'Running') {
+        $alertText = "Started $serviceToStart"
+
+        # notify prtg everything is now clear, but a service was also restarted
+        # due to the freq this script likely runs, you'll probably never know, so
+        # consider alternate means of monitoring crashed services; like extending
+        # debugger() to a new, custom function, if needed
+        prtgApi $alertText 0
+      }
+    }
+  }
+}
